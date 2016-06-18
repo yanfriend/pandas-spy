ca7f536094db5aa4a1a6acb8b8e3ad9dad5446df
Yanzhi Bai <baifriend@Yanzhis-MacBook-Pro.local>
2016-06-17 23:53:26 -0700

    right yaxis

--- sp500_draw.py
+++ sp500_draw.py
@@ -15,6 +15,7 @@ sp_close = sp['Adj Close']
 sp_close = sp_close[interested_range:]
 
 ax1 = plt.subplot(3,1,1)
+ax1.yaxis.tick_right()
 ax1.plot(sp_close, label='sp500')
 plt.legend(loc='upper left')
 
@@ -22,8 +23,9 @@ vix = pd.read_csv('vix.csv', index_col='Date', parse_dates=True)
 vix_close = vix['Adj Close']
 vix_close = vix_close[interested_range:]
 
-ax1 = plt.subplot(3,1,2)
-ax1.plot(vix_close, label='vix')
+ax2 = plt.subplot(3,1,2)
+ax2.yaxis.tick_right()
+ax2.plot(vix_close, label='vix')
 plt.legend(loc='upper left')
 
 pcr = pd.read_csv('equitypc.csv', index_col='DATE', parse_dates=True, skiprows=2)
@@ -32,8 +34,9 @@ pcr = pcr['P/C Ratio']
 ma = pd.rolling_mean(pcr, 13)
 ma = ma[interested_range:]
 
-ax2 = plt.subplot(3,1,3, sharex=ax1)
-ax2.plot(ma, label='pcr ma')
+ax3 = plt.subplot(3,1,3, sharex=ax1)
+ax3.yaxis.tick_right()
+ax3.plot(ma, label='pcr ma')
 plt.legend(loc='upper left')
 
 
