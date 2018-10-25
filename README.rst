7fb12ac400a0733e656a9a1be88ab42db7221294
yanfriend <yanfriend@gmail.com>
2018-10-25 16:06:55 -0700

    download lib works again

--- README.rst
+++ README.rst
@@ -1,3 +1,10 @@
 Module Repository
 ========================
+download sp500, vix and pcr data; draw the graph to compare
 
+python sp500_download.py
+python sp500_draw.py
+
+
+
+https://github.com/ranaroussi/fix-yahoo-finance has a fix to yahoo data download, before pandas fix
--- sp500_draw.py
+++ sp500_draw.py
@@ -33,7 +33,9 @@ pcr = pd.read_csv('equitypc.csv', index_col='DATE', parse_dates=True, skiprows=2
 pcr = pcr['P/C Ratio']
 print pcr.tail(13)
 
-ma = pd.rolling_mean(pcr, 13)
+# ma = pd.rolling_mean(pcr, 13)
+ma=pcr.rolling(13).mean()
+
 ma = ma[interested_range:]
 
 ax3 = plt.subplot(3,1,3, sharex=ax1)
